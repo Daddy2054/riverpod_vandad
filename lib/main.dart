5befaa34aab989a92b6831969de63a01f60bc77a
Jean <acalto2@gmail.com>
2023-05-04 23:06:29 +0200

    example2

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,36 @@ class MyApp extends StatelessWidget {
   }
 }
 
-final currentDate = Provider<DateTime>(
-  (ref) => DateTime.now(),
+// final currentDate = Provider<DateTime>(
+//   (ref) => DateTime.now(),
+// );
+
+extension OptionalInfixAddition<T extends num> on T? {
+  T? operator +(T? other) {
+    final shadow = this;
+    if (shadow != null) {
+      return shadow + (other ?? 0) as T;
+    } else {
+      return null;
+    }
+  }
+}
+
+void testIt() {
+  final int? int1 = 1;
+  final int? int2 = 1;
+  final result = int1 + int2;
+  print(result);
+}
+
+class Counter extends StateNotifier<int?> {
+  Counter() : super(null);
+  void increment() => state = state == null ? 1 : state + 1;
+ // int? get value => state;
+}
+
+final counterProvider = StateNotifierProvider<Counter, int?>(
+  (ref) => Counter(),
 );
 
 class HomePage extends ConsumerWidget {
@@ -34,16 +62,33 @@ class HomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final date = ref.watch(currentDate);
+    testIt();
     return Scaffold(
         appBar: AppBar(
-          title: Text('Hooks Riverpod'),
+          centerTitle: true,
+          title: Consumer(
+              builder: (context, ref, child) {
+                final count = ref.watch(counterProvider);
+                final text =
+                    count == null ? 'Press the button' : count.toString();
+                return Text(text);
+              },
+              child: Text('Hooks Riverpod')),
         ),
-        body: Center(
-          child: Text(
-            date.toIso8601String(),
-            style: Theme.of(context).textTheme.headlineMedium,
-          ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextButton(
+              onPressed: ref.read(counterProvider.notifier).increment,
+              child: Text(
+                'Increment counter',
+              ),
+            )
+          ],
+          // child: Text(
+          //   date.toIso8601String(),
+          //   style: Theme.of(context).textTheme.headlineMedium,
+          // ),
         ));
   }
 }
